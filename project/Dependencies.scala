6f606491230fc490b539194d10337e8781a7b5c4
The Bridge Scorekeeper <35344976+thebridsk@users.noreply.github.com>
2019-06-28 21:56:02 -0400

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,14 +18,14 @@ object Dependencies {
     verScalaVersion.substring(0, i2)
   }
 
-  lazy val verCrossScalaVersions = Seq("2.11.8", verScalaVersion)
+  lazy val verCrossScalaVersions = Seq("2.12.8", verScalaVersion)
 
-  lazy val vScalactic = "3.0.7"      // https://mvnrepository.com/artifact/org.scalactic/scalactic_2.12
-  lazy val vScalatest = "3.0.7"      // http://mvnrepository.com/artifact/org.scalatest/scalatest_2.11
+  lazy val vScalactic = "3.0.8"      // https://mvnrepository.com/artifact/org.scalactic/scalactic_2.12
+  lazy val vScalatest = "3.0.8"      // http://mvnrepository.com/artifact/org.scalatest/scalatest_2.11
   lazy val vJunit = "4.12"           // http://mvnrepository.com/artifact/junit/junit
 
   lazy val vScalaArm = "2.0"         // http://mvnrepository.com/artifact/com.jsuereth/scala-arm_2.11
-  lazy val vScallop = "3.3.0"        // http://mvnrepository.com/artifact/org.rogach/scallop_2.11
+  lazy val vScallop = "3.3.1"        // http://mvnrepository.com/artifact/org.rogach/scallop_2.11
 
 
   // libraries
