7beee6db236ddba034b7ebdebec7ad58b00bca19
The Bridge Scorekeeper <35344976+thebridsk@users.noreply.github.com>
2019-09-25 10:44:12 -0400

    no scala-arm

--- project/BldVersions.scala
+++ project/BldVersions.scala
@@ -15,7 +15,6 @@ object BldVersion {
   lazy val vScalatest = "3.0.8"      // https://github.com/scalatest/scalatest
   lazy val vJunit = "4.12"           // https://github.com/junit-team/junit4
 
-  lazy val vScalaArm = "2.0"         // https://github.com/jsuereth/scala-arm
   lazy val vScallop = "3.3.1"        // https://github.com/scallop/scallop
 
 }
