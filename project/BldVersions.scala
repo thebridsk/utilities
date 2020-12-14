a8d6ca75b013610f70bf841d86d80269204d62e6
The Bridge Scorekeeper <35344976+thebridsk@users.noreply.github.com>
2020-12-14 12:13:30 -0500

    dependencies

--- project/BldVersions.scala
+++ project/BldVersions.scala
@@ -2,7 +2,7 @@
 
 object BldVersion {
 
-  lazy val verScalaVersion = "2.13.3"
+  lazy val verScalaVersion = "2.13.4"
   lazy val verScalaMajorMinor = {
     val i = verScalaVersion.indexOf('.')
     val i2 = verScalaVersion.indexOf('.', i+1)
@@ -11,8 +11,8 @@ object BldVersion {
 
   lazy val verCrossScalaVersions = Seq(verScalaVersion)
 
-  lazy val vScalactic = "3.2.2"      // https://github.com/scalatest/scalatest
-  lazy val vScalatest = "3.2.2"      // https://github.com/scalatest/scalatest
+  lazy val vScalactic = "3.2.3"      // https://github.com/scalatest/scalatest
+  lazy val vScalatest = "3.2.3"      // https://github.com/scalatest/scalatest
   lazy val vJunit = "4.13.1"         // https://github.com/junit-team/junit4
 
   lazy val vScallop = "3.5.1"        // https://github.com/scallop/scallop
